e5d119d8680cc4b64db1c726813b3d07963c8ac6
beatrice <beatrice.mwenje@student.moringaschool>
2023-05-27 23:01:41 +0300

    Done

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,4 +1,14 @@
 def plus_two(num)
-	num + 2
-	num
+    num + 2
+    binding.pry
+    num
 end
+
+require 'pry'
+
+def plus_two(num)
+num* num* num 
+puts "The answer is # {num}"
+end  
+
+plus_two (3)
